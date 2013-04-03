be86b0489555e07d584bdd816d146ee53444f20e
Jay Weisskopf <jay@jayschwa.net>
2013-04-03 15:56:43 -0400

    Switch to ImmutableMatrix types

--- src/uniform.jl
+++ src/uniform.jl
@@ -29,48 +29,42 @@ for (T,t) in ((Float32, "f"), (Int32, "i"), (Uint32, "ui"))
 				(GLint, GLsizei, Ptr{Vector4{$T}}), u.location, 1, &val)
 
 		# Write matrix
-		function write(u::Uniform, mat::Matrix{$T})
-			sz = size(mat)
-			if sz == (2,2)
-				ccall(($(string("glUniformMatrix2", t, "v")), lib), Void,
-					(GLint, GLsizei, GLboolean, Ptr{$T}),
-					u.location, 1, false, mat)
-			elseif sz == (2,3)
-				ccall(($(string("glUniformMatrix2x3", t, "v")), lib), Void,
-					(GLint, GLsizei, GLboolean, Ptr{$T}),
-					u.location, 1, false, mat)
-			elseif sz == (2,4)
-				ccall(($(string("glUniformMatrix2x4", t, "v")), lib), Void,
-					(GLint, GLsizei, GLboolean, Ptr{$T}),
-					u.location, 1, false, mat)
-			elseif sz == (3,2)
-				ccall(($(string("glUniformMatrix3x2", t, "v")), lib), Void,
-					(GLint, GLsizei, GLboolean, Ptr{$T}),
-					u.location, 1, false, mat)
-			elseif sz == (3,3)
-				ccall(($(string("glUniformMatrix3", t, "v")), lib), Void,
-					(GLint, GLsizei, GLboolean, Ptr{$T}),
-					u.location, 1, false, mat)
-			elseif sz == (3,4)
-				ccall(($(string("glUniformMatrix3x4", t, "v")), lib), Void,
-					(GLint, GLsizei, GLboolean, Ptr{$T}),
-					u.location, 1, false, mat)
-			elseif sz == (4,2)
-				ccall(($(string("glUniformMatrix4x2", t, "v")), lib), Void,
-					(GLint, GLsizei, GLboolean, Ptr{$T}),
-					u.location, 1, false, mat)
-			elseif sz == (4,3)
-				ccall(($(string("glUniformMatrix4x3", t, "v")), lib), Void,
-					(GLint, GLsizei, GLboolean, Ptr{$T}),
-					u.location, 1, false, mat)
-			elseif sz == (4,4)
-				ccall(($(string("glUniformMatrix4", t, "v")), lib), Void,
-					(GLint, GLsizei, GLboolean, Ptr{$T}),
-					u.location, 1, false, mat)
-			else
-				error("matrix size ", sz, " is not supported")
-			end
-		end
+		write(u::Uniform, mat::Matrix2x2{$T}) =
+			ccall(($(string("glUniformMatrix2", t, "v")), lib), Void,
+				(GLint, GLsizei, GLboolean, Ptr{Matrix2x2{$T}}),
+				u.location, 1, false, &mat)
+		write(u::Uniform, mat::Matrix2x3{$T}) =
+			ccall(($(string("glUniformMatrix2x3", t, "v")), lib), Void,
+				(GLint, GLsizei, GLboolean, Ptr{Matrix2x3{$T}}),
+				u.location, 1, false, &mat)
+		write(u::Uniform, mat::Matrix2x4{$T}) =
+			ccall(($(string("glUniformMatrix2x4", t, "v")), lib), Void,
+				(GLint, GLsizei, GLboolean, Ptr{Matrix2x4{$T}}),
+				u.location, 1, false, &mat)
+		write(u::Uniform, mat::Matrix3x2{$T}) =
+			ccall(($(string("glUniformMatrix3x2", t, "v")), lib), Void,
+				(GLint, GLsizei, GLboolean, Ptr{Matrix3x2{$T}}),
+				u.location, 1, false, &mat)
+		write(u::Uniform, mat::Matrix3x3{$T}) =
+			ccall(($(string("glUniformMatrix3", t, "v")), lib), Void,
+				(GLint, GLsizei, GLboolean, Ptr{Matrix3x3{$T}}),
+				u.location, 1, false, &mat)
+		write(u::Uniform, mat::Matrix3x4{$T}) =
+			ccall(($(string("glUniformMatrix3x4", t, "v")), lib), Void,
+				(GLint, GLsizei, GLboolean, Ptr{Matrix3x4{$T}}),
+				u.location, 1, false, &mat)
+		write(u::Uniform, mat::Matrix4x2{$T}) =
+			ccall(($(string("glUniformMatrix4x2", t, "v")), lib), Void,
+				(GLint, GLsizei, GLboolean, Ptr{Matrix4x2{$T}}),
+				u.location, 1, false, &mat)
+		write(u::Uniform, mat::Matrix4x3{$T}) =
+			ccall(($(string("glUniformMatrix4x3", t, "v")), lib), Void,
+				(GLint, GLsizei, GLboolean, Ptr{Matrix4x3{$T}}),
+				u.location, 1, false, &mat)
+		write(u::Uniform, mat::Matrix4x4{$T}) =
+			ccall(($(string("glUniformMatrix4", t, "v")), lib), Void,
+				(GLint, GLsizei, GLboolean, Ptr{Matrix4x4{$T}}),
+				u.location, 1, false, &mat)
 	end
 end
 
